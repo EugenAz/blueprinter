9629806b73c8470618805f2082e8f51dd6e3f93a
Eugen Aznaurov <eugen.aznaurov@gmail.com>
2017-11-27 11:10:31 +0100

    Move TODOs to github issues

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-// TODO
-// tests
-// README
-// validations for user config
-// refactor generate command
-// feedback on generation of files
-
 const start = require('./src/main');
 
 start();
